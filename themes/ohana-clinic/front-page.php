630abec2d1dac27053c8b9c985d2a9f088bce7c4
yimm <bunyimkong@gmail.com>
2025-12-23 23:17:57 +0700

    finished hero-section

--- themes/ohana-clinic/front-page.php
+++ themes/ohana-clinic/front-page.php
@@ -2,18 +2,61 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css">
+
+
 <?php get_header(); ?>
 
-<div class="container hero-section">
+<section class="container hero-section">
     <div class="hero-content">
+
         <div class="hero-infor">
             <h1>Warm medical care that fully acknowledges the feelings of each patient</h1>
 
             <p>We will answer your questions thoroughly during your consultation, so
             you can approach your infertility treatment in a relaxed atmosphere.</p>
+
+            <a href="">Discover More</a>
         </div>
+
         <div class="hero-image">
-            <img src="<?php echo get_template_directory_uri();?>/asset/image/hero-section.webp" alt="">
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="<?php echo get_template_directory_uri(); ?>/asset/image/hero-section-3.webp" alt="">
+                    </div>
+
+                    <div class="swiper-slide">
+                        <img src="<?php echo get_template_directory_uri(); ?>/asset/image/hero-section-2.webp" alt="">
+                    </div>
+
+                    <div class="swiper-slide">
+                        <img src="<?php echo get_template_directory_uri(); ?>/asset/image/hero-section.webp" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
+
     </div>    
-</div>
+</section>
+
+<section class="">
+    
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
+
+<script>
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        speed: 500,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+</script>
