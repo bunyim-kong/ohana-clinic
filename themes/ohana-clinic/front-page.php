c2dbee76ceb26f848986dcb53df37528adbb0d55
bunyim-kong <bunyimkong@gmail.com>
2025-12-25 16:34:40 +0700

    Strength Section

--- themes/ohana-clinic/front-page.php
+++ themes/ohana-clinic/front-page.php
@@ -40,9 +40,75 @@
     </div>    
 </section>
 
-<section class="">
-    
+<section class="container strength-section">
+    <h1>Our strengths</h1>
+
+    <div class="strength-content">
+
+        <div class="strength-card">
+            <i class="material-symbols-outlined">browse_gallery</i>
+
+            <h1>Easy Access While You Work</h1>
+
+            <p>Close to the station, open 8:00 AM–8:00 PM (Mon, Wed, Fri)</p>
+
+            <a href="">See more <i class="fas fa-chevron-right"></i></a>
+        </div>
+
+        <div class="strength-card">
+            <i class="material-symbols-outlined">add_notes</i>
+
+            <h1>Proven Pregnancy Results Hahah</h1>
+
+            <p>Pregnancy rate from 2022 to 2023: 63.2% (frozen blastocyst transfer)</p>
+
+            <a href="">See more <i class="fas fa-chevron-right"></i></a>
+        </div>
+
+        <div class="strength-card">
+            <i class="material-symbols-outlined">stethoscope</i>
+
+            <h1>Consistent Care by One Doctor</h1>
+
+            <p>There is no difference in the treatment plan or explanation given by the doctor.</p>
+
+            <a href="">See more <i class="fas fa-chevron-right"></i></a>
+        </div>
+
+    </div>    
 </section>
+<!-- <section class="container strength-section">
+    <h1>Our strengths</h1>
+
+    <div class="stength-content">
+        <div class="strength-items-card">
+            <i class="material-symbols-outlined">browse_gallery</i>
+
+            <h1>Easy Access While You Work</h1>
+
+            <p>Close to the station, open 8:00 AM–8:00 PM (Mon, Wed, Fri)</p>
+
+            <a href="">See more <i class="fas fa-chevron-right"></i></a>
+        </div>
+
+        <div class="strength-items-card">
+            <i class="material-symbols-outlined">browse_gallery</i>
+
+            <h1>Easy Access While You Work</h1>
+
+            <p>Close to the station, open 8:00 AM–8:00 PM (Mon, Wed, Fri)</p>
+
+            <a href="">See more <i class="fas fa-chevron-right"></i></a>
+        </div>
+    </div>
+</section> -->
+
+
+
+
+
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
 
