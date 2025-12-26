f43c902726fe304fafffffe6aadd06ca06c14578
sreanlita81-cpu <sreanlita81@gmail.com>
2025-12-26 16:14:15 +0700

    this 20%

---
+++ themes/ohana-clinic/page-access-to-clinic.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/pages/access-to-clinic.css">
+
+<?php get_header();?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+
+        <section class="biu">
+        <div class="vin">
+            <div class="mlo">
+            <h1 >Access and Clinic Hours</h1>
+            </div>
+        <div class="bo1">
+            <samp>Access and Clinic Hours</samp>
+
+        </div>
+        </section>
+        <section class="tin">
+        <div class="tan">
+            <h1>Access</h1>
+        </div>
+        </section>
+
+        <table class="clinic-table">
+        <tr>
+            <th class="label-cell">Name</th>
+            <td class="content-cell">Keiai Clinic</td>
+        </tr>
+        <tr>
+            <th class="label-cell">Address</th>
+            <td class="content-cell">
+            〒170-0013<br>
+            Fujigi Building 4F, 1-12-8 Higashi-Ikebukuro, Toshima-ku, Tokyo<br>
+            <a href="#" class="map-link">Google Maps</a>
+            </td>
+        </tr>
+        <tr>
+            <th class="label-cell">Phone number</th>
+            <td class="content-cell"><a href="tel:03-3987-3090" class="phone-link">03-3987-3090</a></td>
+        </tr>
+        <tr>
+            <th class="label-cell">Access</th>
+            <td class="content-cell">A one-minute walk from Ikebukuro Station Exit 35.</td>
+        </tr>
+        </table>
+
+        <section class="bio">
+            <div class="lok">
+                <h1>Directions</h1>
+            </div>
+        </section>
+
+
