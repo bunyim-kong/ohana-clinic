33c57df3dc0b3573c920dd3c02831cb764a05cbd
yimm <bunyimkong@gmail.com>
2025-12-28 23:30:53 +0700

    About page 30%

--- themes/ohana-clinic/page-about-us.php
+++ themes/ohana-clinic/page-about-us.php
@@ -6,8 +6,75 @@
 
 <?php get_header(); ?>
 
-<section class="container about-infor-cols">
-    <div class="about-infor-content">
-        <h1>About us</h1>
+<section class="container about-section">
+
+    <div class="about-infor-cols">
+        <div class="head-title">
+            <h1>About us</h1>
+        </div>
+
+        <div class="quote-clinic">
+            <h1><i>Our hospital philosophy</i></h1>
+        </div>
+
+        <div class="about-content">
+
+            <div class="infor-content">
+                <!-- Easy-to-access -->
+                <h2>Easy-to-access medical system</h2>
+
+
+                <div class="infor-txt">
+                    <p>Located one minute from Ikebukuro Exit 35, our clinic is open from 8:00 AM to 8:00 PM on weekdays, weekends, and holidays. We offer flexible systems based on patient feedback, including early-morning consultations completed by 9:00 AM, same-day phone results after exams, and extended evening services to minimize time off work.</p>
+                </div>
+
+                <!-- Doctor's visit -->
+                <h2>Same doctor's visit</h2>
+
+
+                <div class="infor-txt">
+                    <p>At our clinic, all treatment plans, examinations, and surgeries are handled by the director, ensuring consistency and clear explanations. We value patient input and foster an environment where open, comfortable communication is encouraged.</p>
+                </div>
+                
+                <!-- New testing and technology -->
+                <h2>New testing and technology</h2>
+
+
+                <div class="infor-txt">
+                    <p>We stay up to date with the latest medical knowledge by actively participating in academic conferences and sharing insights across our team. By adopting effective tests and treatments and maintaining an optimal incubation environment, we work together to support as many patients as possible in achieving pregnancy.</p>
+                </div>
+
+                <!-- System -->
+                <h2>Counseling System</h2>
+
+
+                <div class="infor-txt">
+                    <p>Our team includes certified infertility counselors and midwife-qualified nurses, ready to discuss a wide range of concerns. We listen closely to our patients and are always here to support you.</p>
+                </div>
+            </div>
+
+            <div class="image-grid">
+                <img src="<?php echo get_template_directory_uri(); ?>/asset/image/location.jpg" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/asset/image/system.jpg" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/asset/image/tech.jpg" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/asset/image/doctor.jpg" alt="">
+            </div>
+
+        </div>
+        
+        <!-- <div class="about-infor-content">
+            <div class="infor-text-cols">
+                <h1>Easy-to-access medical system</h1>
+
+                <p>Located one minute from Ikebukuro Exit 35, our clinic is open from 8:00 AM to 8:00 PM on weekdays, 
+                weekends, and holidays. We offer flexible systems based on patient feedback, including early-morning 
+                consultations completed by 9:00 AM, same-day phone results after exams, and extended evening services 
+                to minimize time off work.</p>
+            </div>
+
+            <div class="infor-image-cols">
+                <img src="<?php echo get_template_directory_uri(); ?>/asset/image/location.jpg" alt="">
+            </div>
+        </div> -->
     </div>
 </section>
