66cb58ecefea07691db16fac2cace6faa066593b
bunyim-kong <bunyimkong@gmail.com>
2025-12-30 08:34:42 +0700

    Medical page recheck

--- themes/ohana-clinic/page-medical.php
+++ themes/ohana-clinic/page-medical.php
@@ -24,6 +24,8 @@
 
                     <p>Pre-embrydermal chromosome heterogeneity test (PGT-A) is a test to examine the number of chromosomes in the embryo. PGT-SR is a test that examines the structure (shape and sequence) of chromosomes in the embryo. Cells are collected from blastocysts obtained by in vitro fertilization and performed. If the number of chromosomes is less or more than usual, or if there is a difference in structure, the embryo may not grow well and may not implant, or it may miscarry even if it is implanted. The aim is to reduce the risk of miscarriage by removing embryos from transplant candidates that are likely to miscarry due to excess or structural abnormalities in chromosomes and structural abnormalities by testing.</p>
 
+                    <h2>The cost</h2>
+
                     <table class="price-table">
                         <thead>
                             <tr>
@@ -73,19 +75,19 @@
                                 <td>A portion of the blastocyst is taken and the blastocyst is frozen. The collected cells are sent to the testing center.</td>
                             </tr>
                             <tr>
-                                <th>6</th>
+                                <th>5</th>
                                 <td>You will be visited by an explanation of the frozen embryo and a consultation after egg collection.</td>
                             </tr>
                             <tr>
-                                <th>7</th>
+                                <th>6</th>
                                 <td>We will contact you as soon as the analysis result is received from the inspection center. Please make an appointment for counseling after the test with your spouse.</td>
                             </tr>
                             <tr>
-                                <th>8</th>
+                                <th>7</th>
                                 <td>Your spouse will come to the hospital with counseling after the examination.</td>
                             </tr>
                             <tr>
-                                <th>9</th>
+                                <th>8</th>
                                 <td><strong>Transplantation</strong></td>
                             </tr>
                         </tbody>
@@ -95,33 +97,6 @@
 
 
             <div class="medical-content">
-                <div class="medical-text">
-                    <h1>(PGT-A) / PGT-SR</h1>
-
-                    <p>Pre-embrydermal chromosome heterogeneity test (PGT-A) is a test to examine the number of chromosomes in the embryo. PGT-SR is a test that examines the structure (shape and sequence) of chromosomes in the embryo. Cells are collected from blastocysts obtained by in vitro fertilization and performed. If the number of chromosomes is less or more than usual, or if there is a difference in structure, the embryo may not grow well and may not implant, or it may miscarry even if it is implanted. The aim is to reduce the risk of miscarriage by removing embryos from transplant candidates that are likely to miscarry due to excess or structural abnormalities in chromosomes and structural abnormalities by testing.</p>
-
-                    <table class="price-table">
-                        <thead>
-                            <tr>
-                                <th>Service</th>
-                                <th>Unit</th>
-                                <th>Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>PGT-A</td>
-                                <td>per person</td>
-                                <td>110,000 yen</td>
-                            </tr>
-                            <tr>
-                                <td>PGT-SR</td>
-                                <td>per person</td>
-                                <td>154,000 yen</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
 
                 <div class="medical-price">
                     <table class="steps-table">
@@ -134,39 +109,69 @@
                         <tbody>
                             <tr>
                                 <th>1</th>
-                                <td>Please watch the explanation video of PGT-A / PGT-SR with your husband and wife, and fill in the video viewing confirmation sheet to be handed over from our hospital. Please make an appointment for counseling before the test.</td>
+                                <td>Before visiting our hospital, please watch the video from the Japan Society of Obstetrics and Gynecology and make an appointment for your first consultation.</td>
                             </tr>
                             <tr>
                                 <th>2</th>
-                                <td>Your spouse will come to the clinic for counseling before the test. Please submit a video viewing confirmation sheet on the day. We will give you a consent form, so please submit it on the next visit to the hospital.</td>
+                                <td>Your doctor will explain the outline of egg freezing and confirm your intentions. After that, we will conduct the necessary tests and provide you with an explanation of egg freezing and a consent form. After you submit the consent form, we will decide on the stimulation method and begin treatment.</td>
                             </tr>
                             <tr>
                                 <th>3</th>
-                                <td><strong>Egg-picking</strong></td>
+                                <td><strong>Egg collection</strong></td>
                             </tr>
                             <tr>
                                 <th>4</th>
-                                <td>A portion of the blastocyst is taken and the blastocyst is frozen. The collected cells are sent to the testing center.</td>
+                                <td>Mature eggs (MII stage) are frozen and stored.</td>
                             </tr>
                             <tr>
-                                <th>6</th>
+                                <th>5</th>
                                 <td>You will be visited by an explanation of the frozen embryo and a consultation after egg collection.</td>
                             </tr>
                             <tr>
+                                <th>6</th>
+                                <td>When the time comes for you to wish to have a child, please contact our clinic. Both you and your spouse will be examined.</td>
+                            </tr>
+                            <tr>
                                 <th>7</th>
-                                <td>We will contact you as soon as the analysis result is received from the inspection center. Please make an appointment for counseling after the test with your spouse.</td>
+                                <td>The eggs are thawed, subjected to in vitro fertilization, and then cultured.</td>
                             </tr>
                             <tr>
                                 <th>8</th>
-                                <td>Your spouse will come to the hospital with counseling after the examination.</td>
-                            </tr>
-                            <tr>
-                                <th>9</th>
-                                <td><strong>Transplantation</strong></td>
+                                <td><strong>Embryo transfer</strong></td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
+
+                <div class="medical-text">
+                    <h1>Egg freezing (social adaptation)</h1>
+
+                    <p>This is done to preserve the fertility of unmarried women. Egg freezing involves freezing eggs (unfertilized eggs) collected from the ovaries, and then thawing them when a woman wishes to become pregnant. The fertilized eggs are then fertilized with sperm and returned to the uterus, allowing for the hope of pregnancy.
+                        The typical survival rate of eggs after freezing and thawing is 90-97%, the fertilization rate after ICSI is 71-79%, the implantation rate is 17-41%, the clinical pregnancy rate per embryo transfer is 36-61%, and the clinical pregnancy rate per thawed egg is 4.5-12%.
+                    </p>
+
+                    <h2>The cost</h2>
+
+                    <table class="price-table">
+                        <thead>
+                            <tr>
+                                <th>Service</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Low stimulation cycle (one to several follicles developing)</td>
+                                <td>220,000 yen</td>
+                            </tr>
+                            <tr>
+                                <td>Stimulated cycle (multiple follicle development)</td>
+                                <td>275,000 yen</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
 
         </div>
